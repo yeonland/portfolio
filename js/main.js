7059106fbbe5eadb237deee0b8975e5d63dad2bc
yeonland <yeonisle@gmail.com>
2026-07-08 10:26:12 +0900

    현장공개 추가

--- js/main.js
+++ js/main.js
@@ -62,6 +62,18 @@ $(function () {
         });
     });
 
+    $('.open_img_btn').click(function() {
+        // 1. 클릭한 버튼의 인덱스 가져오기
+        let index = $(this).data('index');
+
+        // 2. 모든 sheet 숨기고, 선택한 인덱스만 fade 효과로 보여주기
+        $('.sheet').hide().eq(index).fadeIn(500);
+
+        // 3. 버튼 활성화 상태 표시 (선택사항)
+        $('.open_img_btn').removeClass('on');
+        $(this).addClass('on');
+    });
+
     // 탭 전환 기능
     $('.tab-button').click(function() {
         const targetTab = $(this).data('tab')
